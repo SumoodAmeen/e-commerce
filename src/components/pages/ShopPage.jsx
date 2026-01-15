eed7ec204857bdfb36e6b6d0a50f6d2b26de72f4
SumoodAmeen <sumoodameen@gmail.com>
2026-01-15 21:40:36 +0530

    shop page created

---
+++ src/components/pages/ShopPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../home_page/Navbar'
+import Shop from '../shop_page/Shop'
+import Footer from '../home_page/Footer'
+
+const ShopPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <Shop />
+            <Footer />
+        </div>
+    )
+}
+
+export default ShopPage
